7f6b04533f811a2850c0564bc0f1f69e62b35bab
Michael Baldwin <Michael.Baldwin05@gmail.com>
2022-12-12 13:15:08 -0800

    Working

--- src/commands/inquire.ts
+++ src/commands/inquire.ts
@@ -1,4 +1,4 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { ChatInputCommandInteraction, SlashCommandBuilder, Client } from 'discord.js';
 import { Configuration, OpenAIApi } from "openai";
 
 export const data = new SlashCommandBuilder()
@@ -10,25 +10,59 @@ export const data = new SlashCommandBuilder()
             .setDescription("Your question, please :)")
             .setRequired(true));
 
-const configuration = new Configuration({
+const configurationAI = new Configuration({
     organization: process.env.OPENAI_ORGANIZATION_ID,
     apiKey: process.env.OPENAI_SECRET
 });
 
 
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configurationAI);
 
+
+/*
 const completion = async () => {
      await openai.createCompletion({
         model: "text-davinci-002",
         prompt: data.description
     });
     
-} 
+} */
 
 
-export async function execute(interaction: ChatInputCommandInteraction) {
-    var text = "";
+export async function execute(interaction: ChatInputCommandInteraction, client: Client) {
+    if (!interaction.channelId) return;
+    const channel = await client.channels.fetch(interaction.channelId)
+    if (!channel) return;
+    let responseText:string = "";
+    try{
+        const getResponse = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: interaction.options.getString('question'),
+        max_tokens: 1000
+    },
+    {
+        timeout: 10000,
+    })
+    if (!getResponse) interaction.reply("Undefined response");
+    else responseText += getResponse.data.choices[0].text;
+    //responseText += getResponse.data.choices[0].text
+    interaction.reply(responseText)  
+    console.log(responseText)
+    console.log(responseText.length)
+    }
+    catch( error) {
+        interaction.reply(`Error: ${error}`)
+    }
+
+
+}
+
+
+
+
+
+/*
+var text = "";
     (async () => {
         const gptResponse = await openai.createCompletion({
             model: "text-davinci-002",
@@ -40,4 +74,4 @@ export async function execute(interaction: ChatInputCommandInteraction) {
         }
         
     })
-}
+*/
